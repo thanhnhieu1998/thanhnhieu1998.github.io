eaa81c47d1fdf0b05b7f890465a52e24e50b7476
Thanh_Nhieu <nguyenthanhnhieu1998@gmail.com>
2019-12-11 18:43:50 +0700

    upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+ 	var menu = document.querySelectorAll('.navbar.navbar-fixed-top');
+ 	var vitri = "duoi200"
+ 	window.addEventListener("scroll",function(){
+ 		if (window.pageYOffset > 200){
+ 			if (vitri == "duoi200"){
+ 				vitri = "tren200";
+ 				menu[0].classList.add('menutrang');
+ 			}
+ 			
+ 		}
+ 		else if(window.pageYOffset <= 200){
+ 			if(vitri == "tren200"){
+ 				vitri = "duoi200";
+ 				menu[0].classList.remove('menutrang');
+ 			}
+ 		}
+ 	},false)
+ },false)
